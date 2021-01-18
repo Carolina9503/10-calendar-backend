08c056a72b39af353f9e5158ab326e34b1403b0f
Carolina Daza <daza.jeymmy@gmail.com>
2021-01-18 11:01:15 -0500

    320. Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 //npm i express
 const express = require('express'); 
+require('dotenv').config();
+
 
 //crear el servidor de express
 const app = express();
 
-//Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
+//Directorio publico
+app.use( express.static('public'));
 
-})
+
+//Rutas
+// app.get('/', (req, res) => {
+//     res.json({
+//         ok: true
+//     })
+
+// })
 
 
 
 //escuchar peticiones 
-app.listen(4000, () =>{
-    console.log(`Servidor corriendo en el puerto ${ 4000 }`)
+app.listen(process.env.PORT, () =>{
+    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`)
 });
 
 
