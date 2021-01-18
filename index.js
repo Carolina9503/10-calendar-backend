8a1aa66b4b888d96e78ab3a4412ff3ec1644e762
Carolina Daza <daza.jeymmy@gmail.com>
2021-01-18 09:56:24 -0500

    319. Configurando Express

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+//npm i express
+const express = require('express'); 
+
+//crear el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+
+})
 
 
-console.log('Hola mundo')
+
+//escuchar peticiones 
+app.listen(4000, () =>{
+    console.log(`Servidor corriendo en el puerto ${ 4000 }`)
+});
+
+
+
+
+
+
+
